97d2e6b840062d6b5868e97789e5f32f3a52c4ea
Widaazis <52857821+Widaazis@users.noreply.github.com>
2019-07-25 21:48:54 +0700

    Penambahan bilangan array pada Python

---
+++ penambahanBilanganArray.py
@@ -0,0 +1,10 @@
+x = [5, 20 ,30]
+z = [20, 30, 90]
+
+xTotal = sum(x)
+zTotal = sum(z)
+jumlah = xTotal + zTotal
+print("Jumlah Nilai Kedua Array : ", jumlah)
+
+print("Gabungan Array")
+print(x + z)
